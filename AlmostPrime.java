a1e0aac2acbc6136748e01e1817b64db8ed78704
Lavanya <lavanya.kokkiligadda.96@gmail.com>
2019-01-16 20:10:16 +0530

    Almost Prime Program

---
+++ AlmostPrime.java
@@ -0,0 +1,38 @@
+/* Almost prime - A k-Almost-prime is a natural number n that is the product of k (possibly identical) primes.
+
+Example
+1-almost-primes,   where   k=1 ,   are the prime numbers themselves. 
+2-almost-primes,   where   k=2 ,   are the   semiprimes.
+
+Write a function/method/subroutine/... that generates k-almost 
+primes and use it to create a table here of the first ten members of k-Almost primes for 1<=K<=5. */
+
+import java.util.*;
+
+public class AlmostPrime {
+    public static void main(String[] args) {
+        for (int k = 1; k <= 5; k++) {
+            System.out.print("k = " + k + ":");
+ 
+            for (int i = 2, c = 0; c < 10; i++) {
+                if (kprime(i, k)) {
+                    System.out.print(" " + i);
+                    c++;
+                }
+            }
+ 
+            System.out.println("");
+        }
+    }
+ 
+    public static boolean kprime(int n, int k) {
+        int f = 0;
+        for (int p = 2; f < k && p * p <= n; p++) {
+            while (n % p == 0) {
+                n /= p;
+                f++;
+            }
+        }
+        return f + ((n > 1) ? 1 : 0) == k;
+    }
+}
